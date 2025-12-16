ce4b705baa377dee4e53bd6d4d4559797bd93eaa
snischal160@gmail.com <snischal160@gmail.com>
2025-12-16 14:52:19 +0545

    vercel hosting prep

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { Import } from "lucide-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  base: import.meta.env.VITE_BASE_URL || "/",
+});
